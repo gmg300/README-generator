051995807a13e211c3fd735483ddf3325a3ee8a9
Garrett <garrettgassensmith@gmail.com>
2020-04-04 16:07:02 -0500

    Expanded questions array to reflect template categories

--- Develop/index.js
+++ Develop/index.js
@@ -15,11 +15,52 @@ const questions = [
     type: "input",
     name: "title",
     message: "Project Title:"
-  }
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter a description for your project:"
+  },
+  {
+    type: "input",
+    name: "badges",
+    message: "What technology did you use in this project"
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "How do users install your app?"
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "How does your app work?"
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "How can others contribute to your project?"
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Can you describe any tests run for this project?"
+  },
+  {
+    type: "input",
+    name: "credits",
+    message: "Who else worked on this (enter github username), did you use any third party apps or videos?"
+  },
+  {
+    type: "input",
+    name: "license",
+    message: "How would you like to license your project?"
+  },
+
 ];
 
 function writeToFile(fileName, data) {
-  
+
 }
 
 async function init() {
@@ -32,15 +73,7 @@ async function init() {
   // console.log(data);
   const content = generateMarkdown(data);
 
-
-
-  
-  
-
-  
-
-
-
+  writeToFile("README", content);
 }
 
 init();
